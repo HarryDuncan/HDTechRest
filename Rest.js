305358a98cbfab6a6032ddf2ed9f449380750059
HarryDuncan <h.duncan@live.com>
2018-08-31 11:48:50 +1200

    Added Email SES

--- Rest.js
+++ Rest.js
@@ -1,18 +1,21 @@
 var MongoClient = require('mongodb').MongoClient;
 var express = require('express');
-var AWS = require('aws-sdk');
+var aws = require('aws-sdk');
+const bodyParser = require('body-parser');
 const cors = require('cors');
-
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').load();
-}
-
-
 
 //Initialized the REST app
 const app = express();
 app.use(cors());
 
+
+app.use(bodyParser());
+
+
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load();
+}
 
 
 
@@ -43,6 +46,49 @@ app.get('/portfolio', (req, res) =>{
 })
 
 
+// Edit this with YOUR email address.
+var email   = "h.duncan@live.com";
+    
+// Load your AWS credentials and try to instantiate the object.
+aws.config.loadFromPath(__dirname + '/config.json');
+
+// Instantiate SES.
+var ses = new aws.SES();
+
+
+// Sending RAW email
+app.post('/send', function (req, res) {
+	var From = req.body.data.name;
+	var Phone = req.body.data.phone;
+	var Email = req.body.data.email;
+	var Message = req.body.data.message;
+	console.log(From, Phone, Email, Message);
+    var ses_mail = "From: 'Haus Duncan Tech' <" + email + ">\n";
+    ses_mail = ses_mail + "To: " + email + "\n";
+    ses_mail = ses_mail + "MIME-Version: 1.0\n";
+    ses_mail = ses_mail + "Content-Type: multipart/mixed; boundary=\"NextPart\"\n\n";
+    ses_mail = ses_mail + "--NextPart\n";
+    ses_mail = ses_mail + "Content-Type: text/html; charset=us-ascii\n\n";
+    ses_mail = ses_mail + "New message from " + From + " \n" + Phone + " " + Email + " \n";
+    ses_mail = ses_mail + Message;
+    
+    var params = {
+        RawMessage: { Data: new Buffer(ses_mail) },
+        Destinations: [ email ],
+        Source: "'Haus Duncan' <" + email + ">'"
+    };
+    
+    ses.sendRawEmail(params, function(err, data) {
+        if(err) {
+            res.send(err);
+        } 
+        else {
+            res.send(data);
+        }           
+    });
+
+   
+});
 
 
 
